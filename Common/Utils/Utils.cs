8e15f85549e6d7baa85609f9ae2113f94df247c3
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-11-08 14:48:03 +0100

    Added utils

---
+++ Common/Utils/Utils.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Reflection;
+using Godot;
+using Godot.Collections;
+using Array = Godot.Collections.Array;
+
+namespace Composition.Utils;
+
+public static class Utils
+{
+    public static Type GetRootNodeType(this PackedScene scene)
+    {
+        string rootNodeType = scene._Bundled["names"].AsGodotArray<string>()[1];
+        if (rootNodeType == "script")
+        {
+            CSharpScript script = scene._Bundled["variants"].AsGodotArray()[1].Obj as CSharpScript;
+            return script.New().Obj.GetType();
+        }
+        else
+        {
+            return typeof(Node).Assembly.GetType($"Godot.{rootNodeType}");
+        }
+    }
+
+    public static Type GetScriptType(this Script script) => (script as CSharpScript)?.New().Obj?.GetType();
+}
