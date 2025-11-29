48ee322d27e70d2fe6e6fd1203758e5f73d40d32
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-11-29 20:30:06 +0100

    Need a design for UIService

---
+++ UI.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System.Collections.Generic;
+
+public partial class UI : Control
+{
+    private List<Control> _menus = new();
+    private int _currentIndex = 0;
+    
+    public void ShowUI()
+    {
+        
+    }
+
+    public void HideUI()
+    {
+        
+    }
+
+    public void SwitchMenu(Control newMenu)
+    {
+        
+    }
+    
+    public void ReturnToPreviousMenu()
+    {
+        
+    }
+    
+}
