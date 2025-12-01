b9a050b5426af72f47de4b43eb15a2b8053bcafb
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-12-01 16:34:12 +0100

    Added GettingAutoloads

--- Common/Utils/NodeExtension.cs
+++ Common/Utils/NodeExtension.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Godot;
+using Godot.Collections;
 
 namespace Common.Utils;
 
@@ -14,4 +16,21 @@ public static class NodeExtension
         node.SetProcessUnhandledInput(enabled);
         node.SetProcessUnhandledKeyInput(enabled);
     }
+
+    public static T GetAutoloadOfType<T>(this Node node) where T : class
+    {
+        return node.GetTree().GetAutoloadOfType<T>();
+    }
+    
+    public static T GetAutoloadOfType<T>(this SceneTree sceneTree) where T : class
+    {
+        Array<Node> children = sceneTree.Root.GetChildren();
+        foreach (Node child in children)
+        {
+            if(child is T tChild) return tChild;
+        }
+        return null;
+    }
+    
+    
 }
