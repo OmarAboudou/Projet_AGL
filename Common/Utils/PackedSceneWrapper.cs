88eef6cd3fa437c38923f08d45b7bad9846b7c03
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-12-02 18:53:06 +0100

    Made PackedSceneWrapper.cs non Generic to better integrate with Godot

--- Common/Utils/PackedSceneWrapper.cs
+++ Common/Utils/PackedSceneWrapper.cs
@@ -1,27 +1,45 @@
 using System;
+using Common.Log;
 using Godot;
 
 namespace Common.Utils;
 
-[Tool]
-public abstract partial class PackedSceneWrapper<T> : PackedSceneWrapperBase
-where T : class
+[Tool, GlobalClass]
+public abstract partial class PackedSceneWrapper : Resource
 {
-    public sealed override Type GenericType => typeof(T);
+    private PackedScene _scene;
+    protected abstract Type GenericType { get; }
+    static PackedSceneWrapper()
+    {
+        LogSystem.SetLogEnabled<PackedSceneWrapper>(LogType.WARNING, true);
+    }
+
+    public static implicit operator PackedScene(PackedSceneWrapper packedSceneWrapper)
+    {
+        return packedSceneWrapper.Scene;
+    }
     
-    public T Instantiate(PackedScene.GenEditState editState = 0)
+    [Export]
+    public PackedScene Scene
     {
-        return this.Scene.Instantiate<T>(editState);
+        get => this._scene;
+        set
+        {
+            if (value == null)
+            {
+                this._scene = null;
+                return;
+            }
+            
+            Type rootNodeType = value.GetRootNodeType();
+            if (!rootNodeType.IsAssignableTo(GenericType))
+            {
+                LogSystem.Log<PackedSceneWrapper>(LogType.WARNING, $"Scene {value.ResourceName} root node is of type '{rootNodeType.Name}' and is not assignable to type '{GenericType.Name}'");
+                this.NotifyPropertyListChanged();
+                return;
+            }
+            
+            this._scene = value;
+        }
     }
-
-    public T1 Instantiate<T1>(PackedScene.GenEditState editState = 0) where T1 : class
-    {
-        return this.Scene.Instantiate<T1>(editState);
-    }
-
-    public T1 InstantiateOrNull<T1>(PackedScene.GenEditState editState = 0) where T1 : class
-    {
-        return this.Scene.InstantiateOrNull<T1>(editState);
-    }
-
 }
