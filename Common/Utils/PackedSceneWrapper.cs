80b33ae3aab44c3d123b29e5078e8fb387bc60c9
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-12-02 19:55:20 +0100

    Made constructor public

--- Common/Utils/PackedSceneWrapper.cs
+++ Common/Utils/PackedSceneWrapper.cs
@@ -4,12 +4,18 @@ using Godot;
 
 namespace Common.Utils;
 
-public abstract partial class PackedSceneWrapper(Type expectedSceneType) : Resource
+public abstract partial class PackedSceneWrapper : Resource
 {
-    protected PackedSceneWrapper() : this(typeof(object)) { }
+    public PackedSceneWrapper() : this(typeof(object)) { }
 
     private PackedScene _scene;
-    
+    private readonly Type _expectedSceneType;
+
+    public PackedSceneWrapper(Type expectedSceneType)
+    {
+        this._expectedSceneType = expectedSceneType;
+    }
+
     [Export]
     public PackedScene Scene
     {
@@ -23,9 +29,9 @@ public abstract partial class PackedSceneWrapper(Type expectedSceneType) : Resou
             }
             
             Type rootNodeType = value.GetRootNodeType();
-            if (!rootNodeType.IsAssignableTo(expectedSceneType))
+            if (!rootNodeType.IsAssignableTo(this._expectedSceneType))
             {
-                LogSystem.Log<PackedSceneWrapper>(LogType.WARNING, $"Scene {value.ResourceName} root node is of type '{rootNodeType.Name}' and is not assignable to type '{expectedSceneType.Name}'");
+                LogSystem.Log<PackedSceneWrapper>(LogType.WARNING, $"Scene {value.ResourceName} root node is of type '{rootNodeType.Name}' and is not assignable to type '{this._expectedSceneType.Name}'");
                 this.NotifyPropertyListChanged();
                 return;
             }
