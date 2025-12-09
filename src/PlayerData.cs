77a270864d130a459265757863baa60d683eadd4
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-12-09 03:17:26 +0100

    Created a struct for the data of a player that are set from the lobby panel

---
+++ src/PlayerData.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Drawing;
+
+public readonly struct PlayerData(int peerId, String playerName, Color playerColor)
+{
+    public readonly int PeerId = peerId;
+    public readonly string PlayerName = playerName;
+    public readonly Color PlayerColor = playerColor;
+}
