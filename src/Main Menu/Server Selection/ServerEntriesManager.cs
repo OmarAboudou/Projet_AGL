9cfc9a5b7a907c691346bde4c15dc944924f987c
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-12-16 20:26:48 +0100

    Implemented ServerEntriesManager.cs

--- src/Main Menu/Server Selection/ServerEntriesManager.cs
+++ src/Main Menu/Server Selection/ServerEntriesManager.cs
@@ -7,6 +7,27 @@ public partial class ServerEntriesManager : Control
 {
     [Export] private PackedScene _serverEntryScene;
     [Export] private Control _serverEntryRootNode;
+    [Signal]
+    public delegate void ServerEntrySelectedEventHandler(string ipAddress, int port);
+    
+    public void ClearEntries()
+    {
+        foreach (Node child in this._serverEntryRootNode.GetChildren())
+        {
+            if (child is ServerEntry serverEntry)
+            {
+                serverEntry.ServerEntrySelected -= this.EmitSignalServerEntrySelected;
+                serverEntry.QueueFree();
+            }
+        }
+    }
 
+    public void AddEntry(string ipAddress, int port)
+    {
+        ServerEntry serverEntry = this._serverEntryScene.Instantiate<ServerEntry>();
+        serverEntry.Initialize(ipAddress, port);
+        serverEntry.ServerEntrySelected += this.EmitSignalServerEntrySelected;
+        this._serverEntryRootNode.AddChild(serverEntry);
+    }
 
 }
